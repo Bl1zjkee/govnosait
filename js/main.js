74af6b6d8bc2d268665005d2a74803daf6a70c7f
Сергей Новиков <82538900+Bl1zjkee@users.noreply.github.com>
2022-08-20 22:35:33 +0300

    хрен его знает что получлось

--- js/main.js
+++ js/main.js
@@ -4,6 +4,17 @@ new Swiper(".mySwiper", {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
     },
+        pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+        },
+    watchOverflow: true,
+});
+
+
+new Swiper(".mainSwiper", {
+    slidesPerView: 1,
+    spaceBetween:20,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -11,24 +22,106 @@ new Swiper(".mySwiper", {
     watchOverflow: true,
 });
 
-let contactTabs = document.querySelectorAll('.js-tab-contact');
-let inputBlock = document.querySelector('.application__inputs');
-let contactBlock = document.querySelector('.application__contacts');
+new Swiper(".threeSwiper", {
+    slidesPerView: 3,
+    spaceBetween:20,
+    breakpoints:{
+        320:{
+            slidesPerView: 1,
+        },
+        540:{
+            slidesPerView: 2.2,
+        },
 
-contactTabs.forEach((item) => {
-    item.addEventListener('click', function () {
-        item.classList.add('active');
+        1200:{
+            slidesPerView:3,
+        },
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    watchOverflow: true,
+    autoHeight: true,
+});
 
-        let currentBlock = document.querySelectorAll('.js-tab-contact.active');
-        currentBlock.forEach(function (currentBlocks) {
 
-            if (currentBlocks === item) {
+
+
+let tabs = document.querySelectorAll('.js-tab-contact');
+tabs.forEach((item) => {
+    let k = item.dataset.contact;
+    let lols = document.querySelectorAll(`.${k}`);
+
+    item.addEventListener("click",function(e){
+
+        e.preventDefault();
+
+
+        let currentlols = document.querySelectorAll(".js-tab-contact.active");
+        let currentTabs = document.querySelectorAll(".allcard.active");
+
+        currentlols.forEach(function(tabItem){
+            if(tabItem === item){
                 return;
             }
-            console.log(item);
-            currentBlocks.classList.remove('active');
+
+            tabItem.classList.remove("active");
 
         });
+
+        currentTabs.forEach((currentTab) => {
+            currentTab.classList.remove("active");
+
+        });
+
+        lols.forEach((currentItem) => {
+            currentItem.classList.add("active");
+
+
+        });
+
+        item.classList.add("active");
+
+    });
+
+});
+
+
+let tab = document.querySelectorAll('.js-tab-receive');
+tab.forEach((items) => {
+    let t = items.dataset.receive;
+    let lol = document.querySelectorAll(`.${t}`);
+
+    items.addEventListener("click",function(e){
+
+        e.preventDefault();
+
+
+        let currentlol = document.querySelectorAll(".js-tab-receive.active");
+        let currentTab = document.querySelectorAll(".allcards.active");
+
+        currentlol.forEach(function(tabItems){
+            if(tabItems === items){
+                return;
+            }
+
+            tabItems.classList.remove("active");
+
+        });
+
+        currentTab.forEach((currentTabq) => {
+            currentTabq.classList.remove("active");
+
+        });
+
+        lol.forEach((currentItemq) => {
+            currentItemq.classList.add("active");
+
+
+        });
+
+        items.classList.add("active");
 
     });
 
